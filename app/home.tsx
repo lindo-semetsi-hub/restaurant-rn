2684f1bcdba0cdbf56116daec558735c8b884c22
Lindo Semetsi <lindosemetsi@gmail.com>
2026-01-12 10:07:32 +0200

    save

---
+++ app/home.tsx
@@ -0,0 +1,41 @@
+import { Link } from 'expo-router';
+import { FlatList, Text, View } from 'react-native';
+import { menu } from '../data/menu';
+
+export default function Home() {
+  return (
+    <View style={{ padding: 20 }}>
+      <Text style={{ fontSize: 28, fontWeight: 'bold', marginBottom: 20 }}>
+        Menu
+      </Text>
+
+      <FlatList
+        data={menu}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View
+            style={{
+              padding: 15,
+              marginBottom: 15,
+              backgroundColor: '#fff',
+              borderRadius: 10,
+              elevation: 2,
+            }}
+          >
+            <Text style={{ fontSize: 18, fontWeight: 'bold' }}>
+              {item.name}
+            </Text>
+            <Text>{item.description}</Text>
+            <Text style={{ marginTop: 5 }}>R{item.price}</Text>
+
+            <Link href={`/item?id=${item.id}`}>
+              <Text style={{ color: 'red', marginTop: 10 }}>
+                View Item →
+              </Text>
+            </Link>
+          </View>
+        )}
+      />
+    </View>
+  );
+}
---
+++ data/menu.ts
@@ -0,0 +1,32 @@
+export const menu = [
+  {
+    id: '1',
+    category: 'Mains',
+    name: 'Peri-Peri Chicken',
+    description: 'Grilled chicken with peri-peri sauce',
+    price: 120,
+    image:
+     'https://via.placeholder.com/150',
+  },
+
+
+
+
+
+  {
+    id: '2',
+        category: 'Burgers',
+    name: 'Chicken Burger',
+    description: 'Chicken burger with lettuce and mayo',
+    price: 85,
+    image: 'https://via.placeholder.com/150',
+  },
+    {
+        id: '3',
+        category: 'Beverages',
+    name: 'Coke',
+    description: '330ml Coca-Cola',
+    price: 20,
+    image: 'https://via.placeholder.com/150',
+  },
+];
