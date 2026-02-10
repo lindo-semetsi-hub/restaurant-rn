f955f7126fbc6ced9f3e02b0e402c28fb674be50
lindo-semetsi-hub <lindosemetsi@gmail.com>
2026-02-10 11:44:08 +0200

    update menu

--- data/menu.ts
+++ data/menu.ts
@@ -5,7 +5,7 @@ export const menu = [
     name: 'Chicken Liver Roll (Small)',
     description: 'Spicy chicken livers in a fresh roll',
     price: "45",
-    image: 'https://i.imgur.com/1bX5QH6.png',
+    image: 'https://lens.usercontent.google.com/banana?agsi=CmdnbG9iYWw6OjAwMDA1NWNmZWM3MDAyNmQ6MDAwMDAwZWI6MTpiNzMxNGQ4OWYyN2YwYzU3OjAwMDA1NWNmZWM3MDAyNmQ6MDAwMDAxNmQ4OGEwMzM2ODowMDA2NGE3NTEzMmEwZTRmEAI=',
   },
   {
     id: 'roll-livers-large',
